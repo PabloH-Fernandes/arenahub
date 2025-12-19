847fa65d23ed2448bd7af05ef032fe350f8bc7e2
Samuel Aguiar Carvalho Gusmão <acgsamuel92@gmail.com>
2025-12-19 13:10:07 -0300

    style: melhorar o style

--- src/components/User/HeaderUser.jsx
+++ src/components/User/HeaderUser.jsx
@@ -21,42 +21,44 @@ const Header = ({ paginaAtual = 'agendar' }) => {
 
         <div className="logo-nav">
           <img src={logo} className="logo" alt="Logo" />
-          <h2>Arena Hub</h2>
+          <h3>Arena Hub</h3>
         </div>
 
-        {paginaAtual === 'agendar' ? (
-          <div className="intem-nav" onClick={() => window.location.href = '/MeusAgendamentos'}>
-            <Calendar size={22} />
-            <h2>Meus Agendamentos</h2>
-          </div>
-        ) : (
-          <div className="intem-nav" onClick={_route => window.location.href = '/arena/arena-vegas'}>
-            <CalendarCheck size={22} />
-            <h2>Agendar</h2>
-          </div>
-        )}  
-
-        <div className="user-nav">
-          {localStorage.getItem('id_cliente') ? (
-            <>
-              <CircleUserRound size={32} />
-              <h2>{localStorage.getItem('nome_cliente') && localStorage.getItem('nome_cliente') !== 'undefined' ? localStorage.getItem('nome_cliente') : 'Cliente'}</h2>
-              <X 
-                size={20} 
-                style={{ cursor: 'pointer', marginLeft: '10px' }} 
-                onClick={() => {
-                  localStorage.removeItem('id_cliente');
-                  localStorage.removeItem('nome_cliente');
-                  window.location.reload();
-                }}
-              />
-            </>
-          ) : (
-            <div className="login-btn" onClick={() => window.location.href = '/loginUser'} style={{ cursor: 'pointer', display: 'flex', alignItems: 'center', gap: '8px' }}>
-              <CircleUserRound size={32} />
-              <h2>Login</h2>
+        <div className='container-nav-user'>
+          {paginaAtual === 'agendar' ? (
+            <div className="intem-nav" onClick={() => window.location.href = '/MeusAgendamentos'}>
+              <Calendar size={22} />
+              <h3>Meus Agendamentos</h3>
             </div>
-          )}
+          ) : (
+            <div className="intem-nav" onClick={_route => window.location.href = '/arena/arena-vegas'}>
+              <CalendarCheck size={22} />
+              <h3>Agendar</h3>
+            </div>
+          )}  
+
+          <div className="user-nav">
+            {localStorage.getItem('id_cliente') ? (
+              <>
+                <CircleUserRound size={32} />
+                <h3>{localStorage.getItem('nome_cliente') && localStorage.getItem('nome_cliente') !== 'undefined' ? localStorage.getItem('nome_cliente') : 'Cliente'}</h3>
+                <X 
+                  size={20} 
+                  style={{ cursor: 'pointer', marginLeft: '10px' }} 
+                  onClick={() => {
+                    localStorage.removeItem('id_cliente');
+                    localStorage.removeItem('nome_cliente');
+                    window.location.reload();
+                  }}
+                />
+              </>
+            ) : (
+              <div className="login-btn" onClick={() => window.location.href = '/loginUser'} style={{ cursor: 'pointer', display: 'flex', alignItems: 'center', gap: '8px' }}>
+                <CircleUserRound size={32} />
+                <h3>Login</h3>
+              </div>
+            )}
+          </div>
         </div>
       </div>
   );
